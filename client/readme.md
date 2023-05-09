5ed39a8d0f28df002c8c5e7c37bbec5c10fa1383
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-05-10 00:04:00 +0800

    Update form field, add buttons

---
+++ client/readme.md
@@ -0,0 +1,2 @@
+ok start at backend
+1 hr left
---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Card from "./Card.jsx";
+import Loader from "./Loader.jsx";
+import FormField from "./FormField.jsx";
+
+export{
+    Card,
+    FormField,
+    Loader
+}
