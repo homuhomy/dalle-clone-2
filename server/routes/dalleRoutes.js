639a1a8fbb9f0cf9aeabca19db204a1cb5116bca
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-05-11 01:55:20 +0800

    bug: cloudinary sometimes doesnt upload new generated image

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,6 @@
-import express from 'express';
+import express from "express";
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
@@ -8,16 +8,16 @@ const router = express.Router();
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
 const openai = new OpenAIApi(configuration);
 
-router.route('/').get((req, res) => {
-    res.status(200).json({ message: 'Hello from DALL-E!' });
+router.route('/').get((req,res) => {
+    res.send('Hello from DALL-E!');
 });
 
-router.route('/').post(async (req, res) => {
-    try {
+router.route('/').post(async (req,res) => {
+    try{
         const { prompt } = req.body;
 
         const aiResponse = await openai.createImage({
@@ -28,10 +28,13 @@ router.route('/').post(async (req, res) => {
         });
 
         const image = aiResponse.data.data[0].b64_json;
-        res.status(200).json({ photo: image });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+
+        //send to frontend
+        res.status(200).json({photo: image});
+
+    } catch (error){
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message)
     }
 });
 
