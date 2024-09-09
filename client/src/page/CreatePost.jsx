3aa5c182ad40b30a7f52e66a55830a2f77222615
homuhomy <74242263+homuhomy@users.noreply.github.com>
2024-09-09 15:16:59 +0800

    fixed

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -60,7 +60,7 @@ const CreatePost = () => {
         if(form.prompt){
             try{
                 setGeneratingImg(true);
-                const response = await fetch('https://dallehom.netlify.app//api/v1/dalle',{
+                const response = await fetch('https://dalle2u.onrender.com/api/v1/dalle',{
                     method: 'POST',
                     headers:{
                         'Content-Type': 'application/json',
